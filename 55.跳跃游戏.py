29c2736e98f3714e81c0c025845cd9ba077f279b
黄云飞 <huangyunfei@huangyunfeideMacBook-Pro.local>
2020-02-17 00:02:05 +0800

    luo xuan ju

---
+++ 55.跳跃游戏.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode.cn id=55 lang=python3
+#
+# [55] 跳跃游戏
+#
+
+# @lc code=start
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        max = 1
+        for i in range(len(nums)):
+            max -= 1
+            if nums[i] > max:
+                max = nums[i]
+            if max == 0 and i < len(nums) - 1:
+                return False
+        return True
+
+# @lc code=end
+
